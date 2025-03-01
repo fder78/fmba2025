5cec05152193dbde045ad927b262bd5f113b3f72
fder78 <khhwang78@gmail.com>
2025-03-01 18:35:34 +0900

    Update streamlit_app.py
    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,41 @@
 import streamlit as st
+import yfinance as yf
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+@st.cache_data
+def load_stock_prices(tickers, start_date, end_date):
+    data = yf.download(tickers, 
+                       start=start_date, 
+                       end=end_date)['Close']
+    return data
+
+
+# Title
+st.title("Portfolio Dashboard")
+st.subheader("KAIST FMBA 2025 - Personal Project Example", divider=True)
+
+# Sidebar Inputs
+tickers = st.sidebar.multiselect(
+    "WEnter Stock Tickers",
+    ["AAPL", "MSFT", "NVDA", "AMZN", "GOOGL", 
+     "META", "TSLA", "TSM", "BRK-A", "AVGO"],
+    ["AAPL", "NVDA"],
 )
+start_date = st.sidebar.date_input("Start Date", 
+                                   pd.to_datetime("2025-01-01"))
+end_date = st.sidebar.date_input("End Date", 
+                                 pd.to_datetime("2025-12-31"))
+
+normalize = st.sidebar.checkbox("Normalize?")
+
+if st.sidebar.button("Fetch Portfolio Data"):
+    data = load_stock_prices(tickers, start_date, end_date)
+    
+    if normalize:
+        data /= data.iloc[0]
+    st.write("### Portfolio Stock Prices")
+    st.line_chart(data)
+    
+    st.write("### Correlation Matrix")
+    st.dataframe(data.pct_change().corr())
+    
